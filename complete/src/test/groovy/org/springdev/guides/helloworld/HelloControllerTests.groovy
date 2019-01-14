6f7a2d5f5294946fe0278d40423c3dac341a3e73
rainboyan <rainboyan@gmail.com>
2019-01-15 00:34:26 +0800

    Hello World

---
+++ complete/src/test/groovy/org/springdev/guides/helloworld/HelloControllerTests.groovy
@@ -0,0 +1,30 @@
+package org.springdev.guides.helloworld
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+
+@RunWith(SpringRunner.class)
+@SpringBootTest  // <1>
+@AutoConfigureMockMvc
+class HelloControllerTests {
+	@Autowired
+	MockMvc mockMvc
+
+	@Test
+	void hello() throws Exception {
+		this.mockMvc.perform(get("/hello"))  // <2>
+				.andDo(print())
+				.andExpect(status().isOk())  // <3>
+				.andExpect(content().string("Hello, World!"))  // <4>
+	}
+
+}
---
+++ initial/src/main/groovy/org/springdev/guides/helloworld/HelloWorldApplication.groovy
@@ -0,0 +1,14 @@
+package org.springdev.guides.helloworld
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class HelloWorldApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication, args)
+	}
+
+}
+
